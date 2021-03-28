6c6f4d8dc08153eb23b6b07a5d4eab8e3d30b72d
Richard <digitalsadhu@gmail.com>
2021-03-28 20:22:49 +0200

    draw border around grid

--- src/grid.js
+++ src/grid.js
@@ -10,6 +10,42 @@ export default class Grid {
     const { cellsize, gridTransparency, gridColor } = settings;
     const thickness = 1;
     this.layer.removeChildren();
+    this.layer.addChild(
+      line(
+        { x: 0, y: 0 },
+        { x: 0, y: settings.heightPx },
+        thickness,
+        gridTransparency,
+        parseInt(gridColor, 16)
+      )
+    );
+    this.layer.addChild(
+      line(
+        { x: 0, y: 0 },
+        { x: settings.widthPx, y: 0 },
+        thickness,
+        gridTransparency,
+        parseInt(gridColor, 16)
+      )
+    );
+    this.layer.addChild(
+      line(
+        { x: settings.widthPx, y: 0 },
+        { x: settings.widthPx, y: settings.heightPx },
+        thickness,
+        gridTransparency,
+        parseInt(gridColor, 16)
+      )
+    );
+    this.layer.addChild(
+      line(
+        { x: 0, y: settings.heightPx },
+        { x: settings.widthPx, y: settings.heightPx },
+        thickness,
+        gridTransparency,
+        parseInt(gridColor, 16)
+      )
+    );
     for (let x = 1; x < settings.width; x++) {
       this.layer.addChild(
         line(
